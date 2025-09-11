f745fdf1779b4ccca6651dac71d66da9754c5fc7
leegyurak <devgyurak@igyulags-MacBook-Pro.local>
2025-09-11 09:28:36 +0900

    CHG: notice message

--- frontend/src/components/Notice.tsx
+++ frontend/src/components/Notice.tsx
@@ -11,15 +11,15 @@ const Notice: React.FC = () => {
       <div className="notice-content">
         <div className="notice-item">
           <span className="notice-number">1)</span>
-          <span>첫 주문시, 메인메뉴 1개이상 주문 부탁 드립니다.</span>
+          <span>첫 주문 시, 메인메뉴 1개 이상 주문 필수입니다.</span>
         </div>
         <div className="notice-item">
           <span className="notice-number">2)</span>
-          <span>주점 이용시간은 첫 주문시간으로부터 1시간 30분 입니다.</span>
+          <span>주점 이용 시간은 첫 주문 시간으로부터 <strong>1시간 30분</strong> 입니다.</span>
         </div>
         <div className="notice-item">
           <span className="notice-number">3)</span>
-          <span>테이블 전 인원 공석이 10분 지속되는 경우 테이블 정리 진행될 예정입니다.</span>
+          <span>테이블 자리비움은 10분까지 입니다.<br />초과 시 안내 없이 테이블 정리할 예정이오니 자리비움에 유의해 주시기 바랍니다.</span>
         </div>
       </div>
     </div>
