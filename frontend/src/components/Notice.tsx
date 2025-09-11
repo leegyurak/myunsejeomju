a24c5ac037126e91a3682aa95ba54d26b97e09b0
leegyurak <devgyurak@igyulags-MacBook-Pro.local>
2025-09-11 09:31:14 +0900

    ADD: notice message

--- frontend/src/components/Notice.tsx
+++ frontend/src/components/Notice.tsx
@@ -21,6 +21,10 @@ const Notice: React.FC = () => {
           <span className="notice-number">3)</span>
           <span>테이블 자리비움은 10분까지 입니다.<br />초과 시 안내 없이 테이블 정리할 예정이오니 자리비움에 유의해 주시기 바랍니다.</span>
         </div>
+        <div className="notice-item">
+          <span className="notice-number">4)</span>
+          <span>결제 완료 시, 반드시 <strong>결제 확인창으로 돌아와 결제 확인 버튼을 누르셔야 합니다.</strong></span>
+        </div>
       </div>
     </div>
   );
